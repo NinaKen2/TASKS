48dfa7e46bb833f1e6910d64efe0fb543cc8d375
nina ken <orediggers2@mail.ru>
2023-10-01 08:46:58 +0300

    Сщздан репо Tasks

---
+++ Task004/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход три числа
+и на выходе показывает максимальное из этих чисел
+ 2, 3, 7 -> 7 
+14, 5, 78 -> 78 */
+
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Concole.Write("max =");
+Concole.WriteLine(max);
---
+++ Task008/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу которая на вход принимает число (N), 
+а на выходе показывает все четные числаот 1 до N
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i=2;
+
+while(i <=N)
+{ int i = i + 2;
+ Console.WriteLine(i);
+}
